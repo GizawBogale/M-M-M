9f30a4af07016091198809802581988acb5dbc3d
Gizaw <gizawbogale2@gmail.com>
2023-02-12 10:48:35 +0300

    Initial commit

---
+++ Mean.py
@@ -0,0 +1,11 @@
+# findingn the mean of the array
+
+salaries = [4, 6, 7, 8, 10, 15, 20]
+def mean(salaries):
+    sum = 0
+    for i in salaries:
+        sum +=i
+        mean = sum/len(salaries)
+    return mean
+print("the mean of the given array is:",mean(salaries))
+
