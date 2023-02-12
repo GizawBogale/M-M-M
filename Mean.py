95cd535a1a83c3d4000b666fd197a6195bd325e9
Gizaw <gizawbogale2@gmail.com>
2023-02-12 10:53:02 +0300

    Initial 1

--- Mean.py
+++ Mean.py
@@ -7,5 +7,6 @@ def mean(salaries):
         sum +=i
         mean = sum/len(salaries)
     return mean
+print("the mean of the given array is:",mean(salaries)
 print("the mean of the given array is:",mean(salaries))
 
