25bfeeaea976c17e3b46a5ceb64fc1674701d1b2
Gizaw <gizawbogale2@gmail.com>
2023-02-13 14:24:33 +0300

    commit 6

--- Mean.py
+++ Mean.py
@@ -7,7 +7,7 @@ def mean(salaries):
         sum +=i
         mean = sum/len(salaries)
     return mean
-print("The mean of the given array is:",mean(salaries)
+print("The mean of the given array is:",mean(salaries))
 
 
 
