a1faa6f2e88c7c1fb75ddc39de20b46a2e442059
Gizaw <gizawbogale2@gmail.com>
2023-02-13 14:30:10 +0300

    commit 8

--- Mean.py
+++ Mean.py
@@ -1,6 +1,6 @@
 # findingn the mean of the array
 
-salaries = [4, 6, 7, 8, 10, 15, 20]
+salaries = [1250, 1200, 813, 1046, 1046, 806, 952, 927, 915, 861, 850, 814, 813, 809, 808, 806, 792, 778, 757, 755, 813, 752, 750]
 def mean(salaries):
     sum = 0
     for i in salaries:
